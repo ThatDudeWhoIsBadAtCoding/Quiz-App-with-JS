23f3992ccc4dcc6a06a2e2be07fb72517bc3ec50
ThatDudeWhoIsBadAtCoding <90434651+ThatDudeWhoIsBadAtCoding@users.noreply.github.com>
2021-12-12 12:46:42 +0530

    Update index.js

--- in_use/index.js
+++ in_use/index.js
@@ -1,31 +1,19 @@
-let buttons = Array.from(document.getElementsByClassName("home"));
 const fin = document.getElementById("acc");
+let account = JSON.parse(localStorage.getItem("account"));
+if(!account){
+    localStorage.setItem("account", JSON.stringify({type: false}))
+}
 
-buttons.forEach((btn) => {
-    btn.addEventListener('click', e => {
-        if(fin.innerHTML == "You aren't signed into an account yet") return e.preventDefault()
-    })
-})
+
 async function check(){
-let url_string = window.location.href;
-let url = new URL(url_string);
-let name = url.searchParams.get("acc");
-let highscore = url.searchParams.get("highscore");
-if(highscore != null){
-    localStorage.removeItem("highscore")
-    localStorage.setItem("highscore", highscore);
-}
-name =  name == null ? localStorage.getItem("name"): name
-highscore =  highscore == null ? localStorage.getItem("highscore"): localStorage.getItem("highscore") == null ? null: localStorage.getItem("highscore")
-console.log(highscore, name);
-if(name != null){
-    localStorage.removeItem("name")
-    localStorage.setItem("name", name)
-    fin.innerHTML = `You have logged in as ${name}`
-    document.getElementById("high").innerHTML = `Highscore: ${highscore}`
-}
-else{
-    fin.innerHTML == "You aren't signed into an account yet"
-    document.getElementById("high").innerHTML = ''
-}
+    account = JSON.parse(localStorage.getItem("account"));
+    console.log(account);
+    if(account.type){
+        fin.innerHTML = `You have logged in as ${account.name}`
+        document.getElementById("high").innerHTML = `Highscore: ${account.highscore}`
+    }
+    else{
+        fin.innerHTML == "You are logged in as a guest"
+        document.getElementById("high").innerHTML = 'Sign up / Login to save you highscores'
+    }
 }
