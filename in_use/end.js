84c6950ce36e74c32d060f614e5a9e69f05fb843
ThatDudeWhoIsBadAtCoding <90434651+ThatDudeWhoIsBadAtCoding@users.noreply.github.com>
2021-12-12 22:24:55 +0530

    Update end.js

--- in_use/end.js
+++ in_use/end.js
@@ -1,14 +1,15 @@
-const mostRecentScore = localStorage.getItem("mostRecentScore");
 const displayScore = document.getElementById("finalScore");
+const url_str = window.location.href;
+const url = new URL(url_str);
+const diff = url.searchParams.get("diff");
+const mostRecentScore = url.searchParams.get("mostRecentScore");
 
 
 if (mostRecentScore == null || mostRecentScore <= 0){
     displayScore.innerText = "You Scored 0 points :C"
 }
 else {
-    let url_str = window.location.href;
-    const url = new URL(url_str);
-    const diff = url.searchParams.get("diff");
+    
     if(diff == null){
         displayScore.innerText = "Your final score was " + mostRecentScore + " points!\nLog in to save you score!";
     }
