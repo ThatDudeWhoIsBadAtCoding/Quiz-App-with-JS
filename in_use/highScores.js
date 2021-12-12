a8047643a435f1c903f0e8795f03822b4594aa41
ThatDudeWhoIsBadAtCoding <90434651+ThatDudeWhoIsBadAtCoding@users.noreply.github.com>
2021-12-12 13:00:15 +0530

    Update highScores.js

--- in_use/highScores.js
+++ in_use/highScores.js
@@ -1,14 +1,17 @@
 const highScoresList = document.getElementById("highScoresList");
-
-const update = async () => {
-  const re = await fetch("/all")
-  const res = await re.json()
+var dupe;
+const update = async (source) => {
+  const re = await fetch("/all");
+  const res = await re.json();
   let counter = 0;
   res.docs.sort((a, b) => a.highscore - b.highscore);
   let arr = res.docs.reverse();
-  highScoresList.innerHTML = arr.map(score => {
+  dupe = arr.map(score => {
   if(counter >= 5) return
   counter += 1
   return `<li class = "high-scores">${score.user} - ${score.highscore}</li>`
 }).join("")
+  if(source){
+      highScoresList.innerHTML = dupe
+  }
 }
